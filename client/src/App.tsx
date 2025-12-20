5dfe2fd8de2ae9d4336d60ccaf064900c81d22c0
Anna Alfereva <anna.alf@inbox.ru>
2025-12-20 15:38:39 +0100

    add: structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,29 +6,35 @@ import SGLogo from "./assets/icons/smart-garden.svg?react";
 const tools = [
   { label: "Profile", key: "profile" },
   { label: "Nodes", key: "nodes" },
+  { label: "Pots", key: "pots" },
   { label: "Settings", key: "settings" },
 ];
 
 function App() {
-  const [selectedTool, setSelectedTool] = useState("nodes");
+  const [selectedTool, setSelectedTool] = useState("pots");
 
   const handleToolSelect = (toolKey: string) => {
     setSelectedTool(toolKey);
   };
   return (
-    <div>
-      <div></div>
-      <Sidebar
-        tools={tools}
-        selected={selectedTool}
-        onSelect={handleToolSelect}
-      />
-      <footer className="footer-box">
-        <span className="footer-copyright">
-          © 2025 Tým 4 & Unicorn University
-        </span>
-        <SGLogo className="icon" />
-      </footer>
+    <div id="main-container" className="main-container">
+      <div className="header-container">
+        <SGLogo className="header-logo" />
+        <Sidebar
+          tools={tools}
+          selected={selectedTool}
+          onSelect={handleToolSelect}
+        />
+      </div>
+      <div id="content-container" className="content-container">
+        <div id="content-box" className="content-box"></div>
+        <footer className="footer-box">
+          <span className="footer-copyright">
+            © 2025 Tým 4 & Unicorn University
+          </span>
+          <SGLogo className="icon" />
+        </footer>
+      </div>
     </div>
   );
 }
