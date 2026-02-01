7d6e81f1f5155db6c62b873a2dc5080e64095f22
Niraku <matysekDrak+github@gmail.com>
2026-02-01 03:16:52 +0100

    fin

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,11 @@ import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
   plugins: [react(), svgr()],
+  host: "0.0.0.0",
+  port: 5173,
+  hmr: {
+    protocol: "wss",
+    host: "uuiot.cytadel.xyz",
+    clientPort: 443,
+  },
 });
